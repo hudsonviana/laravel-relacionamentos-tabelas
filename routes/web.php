d3f5b141d20a69bdc37087cd81051134ce571a11
hudsonviana <hudson1206@gmail.com>
2022-04-05 22:58:27 -0300

    relacionamento One to One

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,33 @@
 <?php
 
+use App\Models\{
+    User,
+    Preference
+};
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/one-to-one', function () {
+    $user = User::with('preference')->find(2);
+
+    $data = [
+        'background_color' => '#000'
+    ];
+    
+    if ($user->preference) {
+        $user->preference->update($data);
+    } else {
+        // $user->preference()->create($data); // uma forma de se fazer
+        $preference = new Preference($data);
+        $user->preference()->save($preference); // outra forma de se fazer
+    }
+    
+    $user->refresh();
+
+    $user->preference->delete();
+
+    dd($user->preference);
+});
 
 Route::get('/', function () {
     return view('welcome');
